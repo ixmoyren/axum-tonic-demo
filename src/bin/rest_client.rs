51bcc93a4a16da52cd2fb2974560411b90c26fde
ixmoyren <mxonren@foxmail.com>
2025-11-16 23:10:52 +0800

    实现 REST 客户端

---
+++ src/bin/rest_client.rs
@@ -0,0 +1,23 @@
+use serde::Deserialize;
+use tabled::{Table, Tabled};
+
+#[derive(Deserialize, Tabled)]
+struct Task {
+    id: i64,
+    description: String,
+    done: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    let url = "http://127.0.0.1:3000/all";
+    let response = reqwest::get(url).await.unwrap();
+
+    let body = response.text().await.unwrap();
+
+    let todos = serde_json::from_str::<Vec<Task>>(&body).unwrap();
+
+    let table = Table::new(todos);
+
+    println!("{table}");
+}
